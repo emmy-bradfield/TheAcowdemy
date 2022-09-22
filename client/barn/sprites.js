e197a8d83e659758507ccc977c8486ef7dba5870
Emily Bradfield <emily-bradfield@outlook.com>
2022-09-22 20:34:43 +0100

    placeholder for barn

--- client/barn/sprites.js
+++ client/barn/sprites.js
@@ -1,35 +1,7 @@
-const MAP = new Sprite({
-    position:{
-        x: offset.x,
-        y: offset.y
-    },
-    image: Images.map
-});
-
-const PLAYER = new Sprite({
-    position:{
-        x: (canvas.width/2)-(Images.player.width/18),
-        y: (canvas.height/2)-(Images.player.height)
-    },
-    image: Images.player,
-    frames: {
-        max: 9,
-        rate: 5
-    }
-});
-
-const FOREGROUND = new Sprite({
+const BACKGROUND = new Background({
     position: {
-        x: offset.x,
-        y: offset.y
+        x: (canvas.width/2)-(display.width/2),
+        y: (canvas.height/2)-(display.height/2)
     },
-    image: Images.foreground
-});
-
-const UI = new Sprite({
-    position: {
-        x: (canvas.width/2)-(Images.overlay.width/2),
-        y: (canvas.height/2)-(Images.overlay.height/2)
-    },
-    image: Images.overlay
+    image: display
 })
